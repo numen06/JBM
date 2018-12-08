a897ba1605705e2b0929a1939f699fd9da18feb3
numen06 <numen06@sina.com>
2018-12-09 00:15:21 +0800

    独立出例子项目

--- jbm-framework/jbm-framework-sample/jbm-framework-sample-mqtt/src/test/java/com/jbm/test/mqtt/MqttTest.java
+++
@@ -1,9 +0,0 @@
-package com.jbm.test.mqtt;
-
-import junit.framework.TestCase;
-
-public class MqttTest extends TestCase {
-
-	public void testRest() {
-	}
-}
--- jbm-framework/jbm-framework-sample/jbm-framework-sample-thrift/src/test/resources/add.thrift
+++
@@ -1,7 +0,0 @@
-namespace java com.eviac.blog.samples.thrift.server  // defines the namespace   
-  
-typedef i32 int  //typedefs to get convenient names for your types  
-  
-service AdditionService {  // defines the service to add two numbers  
-        int add(1:int n1, 2:int n2), //defines a method  
-}
