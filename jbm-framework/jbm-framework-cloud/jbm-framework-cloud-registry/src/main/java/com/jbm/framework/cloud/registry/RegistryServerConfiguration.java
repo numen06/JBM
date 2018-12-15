d9a0006d3ca4f3550b492880d8862c73cabc3792
numen06 <numen06@sina.com>
2018-12-16 01:38:31 +0800

    添加注册器

---
+++ jbm-framework/jbm-framework-cloud/jbm-framework-cloud-registry/src/main/java/com/jbm/framework/cloud/registry/RegistryServerConfiguration.java
@@ -0,0 +1,21 @@
+package com.jbm.framework.cloud.registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: create by wesley
+ * @date:2018/12/16
+ */
+@EnableEurekaServer
+public class RegistryServerConfiguration implements InitializingBean {
+
+    private Logger logger = LoggerFactory.getLogger(RegistryServerConfiguration.class);
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("jbm eureke registry server start");
+    }
+}
