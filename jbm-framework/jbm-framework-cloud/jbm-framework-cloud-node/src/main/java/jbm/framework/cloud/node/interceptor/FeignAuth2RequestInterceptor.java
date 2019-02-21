9e661b532ea0c117dfa7c70f14ccfcb38cd681e0
zangju2 <125046870@qq.com>
2019-02-21 15:54:41 +0800

    Create FeignAuth2RequestInterceptor.java

---
+++ jbm-framework/jbm-framework-cloud/jbm-framework-cloud-node/src/main/java/jbm/framework/cloud/node/interceptor/FeignAuth2RequestInterceptor.java
@@ -0,0 +1,23 @@
+package jbm.framework.cloud.node.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+
+
+public class FeignAuth2RequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        SecurityContext securityContext= SecurityContextHolder.getContext();
+        Authentication authentication=securityContext.getAuthentication();
+        if(authentication!=null&&authentication.getDetails() instanceof OAuth2AuthenticationDetails ){
+            OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)authentication.getDetails();
+            requestTemplate.header("Authorization",String.format("%s %s","bearer",details.getTokenValue()));
+
+        }
+    }
+}
