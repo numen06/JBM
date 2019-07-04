a5941accd5fd8de4dbddaeef3aa8c82488b67d2c
numen06 <numen06@sina.com>
2019-07-04 21:35:06 +0800

    Update IMasterDataController.java

--- jbm-framework/jbm-framework-data/jbm-framework-data-masterdata/src/main/java/com/jbm/framework/masterdata/controller/IMasterDataController.java
+++ jbm-framework/jbm-framework-data/jbm-framework-data-masterdata/src/main/java/com/jbm/framework/masterdata/controller/IMasterDataController.java
@@ -7,17 +7,51 @@ import com.jbm.framework.usage.form.JsonRequestBody;
 public interface IMasterDataController<Entity extends MasterDataEntity, Service extends IMasterDataService<Entity>> {
 
 
+    /**
+     * 查询列表
+     *
+     * @param jsonRequestBody
+     * @return
+     */
     Object pageList(JsonRequestBody jsonRequestBody);
 
+    /**
+     * 获取单一对对象
+     * @param jsonRequestBody
+     * @return
+     */
     Object model(JsonRequestBody jsonRequestBody);
 
+    /**
+     * 保存单一对象
+     * @param jsonRequestBody
+     * @return
+     */
     Object save(JsonRequestBody jsonRequestBody);
 
+    /**
+     * @param jsonRequestBody
+     * @return
+     */
     Object remove(JsonRequestBody jsonRequestBody);
 
+    /**
+     * 保存多个对象
+     * @param jsonRequestBody
+     * @return
+     */
     Object saveBatch(JsonRequestBody jsonRequestBody);
 
+    /**
+     * 生产假数据
+     * @return
+     */
     Object mock();
 
+    /**
+     * 批量删除
+     * @param jsonRequestBody
+     * @return
+     */
     Object deleteByIds(JsonRequestBody jsonRequestBody);
 }
