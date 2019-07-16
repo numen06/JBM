6e4761ab7b722bcc950cddab60cc6206ef44b6f8
numen06 <numen06@sina.com>
2019-07-16 14:10:58 +0800

    增加分页封装方法

--- jbm-framework/jbm-framework-core/src/main/java/com/jbm/framework/usage/paging/DataPaging.java
+++ jbm-framework/jbm-framework-core/src/main/java/com/jbm/framework/usage/paging/DataPaging.java
@@ -7,119 +7,125 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 
  * 封装分页数据的封装类
- * 
- * @author Wesley
- * 
+ *
  * @param <E>
+ * @author Wesley
  */
 public class DataPaging<E> implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 空的数据
-	 */
-	private final List<E> EMPTY_CONTENT = new ArrayList<>();
-	private final PageForm EMPTY_PAGEABLE = new PageForm();
+    /**
+     * 空的数据
+     */
+    private final List<E> EMPTY_CONTENT = new ArrayList<>();
+    private final PageForm EMPTY_PAGEABLE = new PageForm();
 
-	/**
-	 * 查询的内容
-	 */
-	private List<E> contents = EMPTY_CONTENT;
+    /**
+     * 查询的内容
+     */
+    private List<E> contents = EMPTY_CONTENT;
 
-	/**
-	 * 总数
-	 */
-	private Long total;
+    /**
+     * 总数
+     */
+    private Long total;
 
-	private Long totalPage;
+    private Long totalPage;
 
-	/**
-	 * 查询的分页信息
-	 */
-	private PageForm pageForm = EMPTY_PAGEABLE;
+    /**
+     * 查询的分页信息
+     */
+    private PageForm pageForm = EMPTY_PAGEABLE;
 
-	public DataPaging() {
-		super();
-	}
+    public DataPaging() {
+        super();
+    }
 
-	public DataPaging(List<E> contents, Long total) {
-		super();
-		this.contents = contents;
-		this.total = total;
-	}
+    public DataPaging(List<E> contents, DataPaging dataPaging) {
+        super();
+        this.contents = contents;
+        this.total = dataPaging.getTotal();
+        this.pageForm = dataPaging.getPageForm();
+    }
 
-	public DataPaging(List<E> contents, Long total, Long totalPage, PageForm pageForm) {
-		super();
-		this.contents = contents;
-		this.total = total;
-		this.totalPage = totalPage;
-		this.pageForm = pageForm;
-	}
 
-	public DataPaging(List<E> contents, Long total, PageForm pageForm) {
-		super();
-		this.contents = contents;
-		this.total = total;
-		this.pageForm = pageForm;
-	}
+    public DataPaging(List<E> contents, Long total) {
+        super();
+        this.contents = contents;
+        this.total = total;
+    }
 
-	public PageForm getPageForm() {
-		return pageForm;
-	}
+    public DataPaging(List<E> contents, Long total, Long totalPage, PageForm pageForm) {
+        super();
+        this.contents = contents;
+        this.total = total;
+        this.totalPage = totalPage;
+        this.pageForm = pageForm;
+    }
 
-	public void setPageForm(PageForm pageForm) {
-		this.pageForm = pageForm;
-	}
+    public DataPaging(List<E> contents, Long total, PageForm pageForm) {
+        super();
+        this.contents = contents;
+        this.total = total;
+        this.pageForm = pageForm;
+    }
 
-	public Long getTotal() {
-		return total;
-	}
+    public PageForm getPageForm() {
+        return pageForm;
+    }
 
-	public void setTotal(Long total) {
-		this.total = total;
-	}
+    public void setPageForm(PageForm pageForm) {
+        this.pageForm = pageForm;
+    }
 
-	public List<E> getContents() {
-		return contents;
-	}
+    public Long getTotal() {
+        return total;
+    }
 
-	public void setContents(List<E> contents) {
-		this.contents = contents;
-	}
+    public void setTotal(Long total) {
+        this.total = total;
+    }
 
-	public List<E> EMPTY_CONTENT() {
-		return EMPTY_CONTENT;
-	}
+    public List<E> getContents() {
+        return contents;
+    }
 
-	private Map<String, Object> exposition;
+    public void setContents(List<E> contents) {
+        this.contents = contents;
+    }
 
-	public Map<String, Object> getExposition() {
-		return exposition;
-	}
+    public List<E> EMPTY_CONTENT() {
+        return EMPTY_CONTENT;
+    }
 
-	public <K, V, T extends Map<K, V>> void putExp(Map<String, T> exp) {
-		if (exposition == null)
-			exposition = new HashMap<>();
-		exp.putAll(exp);
-	}
+    private Map<String, Object> exposition;
 
-	public <K, V, T extends Map<K, V>> void putExp(String type, T mapper) {
-		if (exposition == null)
-			exposition = new HashMap<>();
-		exposition.put(type, mapper);
-	}
+    public Map<String, Object> getExposition() {
+        return exposition;
+    }
 
-	public Long getTotalPage() {
-		return totalPage;
-	}
+    public <K, V, T extends Map<K, V>> void putExp(Map<String, T> exp) {
+        if (exposition == null)
+            exposition = new HashMap<>();
+        exp.putAll(exp);
+    }
 
-	public void setTotalPage(Long totalPage) {
-		this.totalPage = totalPage;
-	}
+    public <K, V, T extends Map<K, V>> void putExp(String type, T mapper) {
+        if (exposition == null)
+            exposition = new HashMap<>();
+        exposition.put(type, mapper);
+    }
+
+    public Long getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Long totalPage) {
+        this.totalPage = totalPage;
+    }
 
 }
