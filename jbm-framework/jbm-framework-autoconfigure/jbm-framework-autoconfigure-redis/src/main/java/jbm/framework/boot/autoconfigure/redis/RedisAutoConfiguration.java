cae82ba09c7d5ab51a4d2cb57afe428c5b0f3118
numen06 <numen06@sina.com>
2019-08-07 16:10:03 +0800

    提交redis的默认序列化方式

--- jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-redis/src/main/java/jbm/framework/boot/autoconfigure/redis/RedisAutoConfiguration.java
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-redis/src/main/java/jbm/framework/boot/autoconfigure/redis/RedisAutoConfiguration.java
@@ -1,35 +1,41 @@
 package jbm.framework.boot.autoconfigure.redis;
 
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * 默认的缓存注入
- * 
- * @author wesley
  *
+ * @author wesley
  */
 @Configuration
 @ConditionalOnProperty(prefix = "spring.redis", name = "host")
 @ConditionalOnClass(RedisConnectionFactory.class)
 public class RedisAutoConfiguration {
 
-	@Bean
-	@ConditionalOnMissingBean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-		RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-		template.setConnectionFactory(connectionFactory);
-		// template.setValueSerializer(jackson2JsonRedisSerializer);
-		// template.setKeySerializer(jackson2JsonRedisSerializer);
-		template.afterPropertiesSet();
-		return template;
-	}
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(connectionFactory);
+        //使用json格式化
+        FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
+        // value值的序列化采用fastJsonRedisSerializer
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+        // key的序列化采用StringRedisSerializer
+        template.setKeySerializer(serializer);
+        template.setHashKeySerializer(serializer);
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
 
 }
