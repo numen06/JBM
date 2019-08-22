7185e25e328f278f0a222bb3efc98491b37af889
numen06 <numen06@sina.com>
2019-08-23 00:20:27 +0800

    添加百度OCR模块

---
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-baidu-ocr/src/main/java/jbm/framework/boot/autoconfigure/baidu/ocr/BaiduOcrAutoConfiguration.java
@@ -0,0 +1,29 @@
+package jbm.framework.boot.autoconfigure.baidu.ocr;
+
+import com.baidu.aip.ocr.AipOcr;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(BaiduOcrProperties.class)
+@ConditionalOnProperty(prefix = BaiduOcrProperties.PREFIX)
+public class BaiduOcrAutoConfiguration {
+
+    @Autowired
+    private BaiduOcrProperties baiduOcrProperties;
+
+    @Bean
+    public AipOcr baiduOcr() {
+        // 初始化一个AipOcr
+        AipOcr client = new AipOcr(baiduOcrProperties.getAppId(), baiduOcrProperties.getApiKey(), baiduOcrProperties.getSecretKey());
+        // 可选：设置网络连接参数
+        client.setConnectionTimeoutInMillis(2000);
+        client.setSocketTimeoutInMillis(60000);
+        return client;
+    }
+
+
+}
---
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-baidu-ocr/src/main/java/jbm/framework/boot/autoconfigure/baidu/ocr/BaiduOcrProperties.java
@@ -0,0 +1,18 @@
+package jbm.framework.boot.autoconfigure.baidu.ocr;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = BaiduOcrProperties.PREFIX)
+public class BaiduOcrProperties {
+
+    public static final String PREFIX = "baidu.ocr";
+
+    private String appId;
+    private String apiKey;
+    private String secretKey;
+    private String proxyHost;
+
+
+}
