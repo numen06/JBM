96b7a6647e8d6f19bbcf3d42b78cf9438fc06dbe
numen06 <numen06@sina.com>
2019-09-16 17:02:23 +0800

    修复OSS的问题

--- jbm-framework/jbm-framework-aliyun/jbm-framework-aliyun-oss/src/main/java/jbm/framework/aliyun/oss/AliyunOSSTemplate.java
+++ jbm-framework/jbm-framework-aliyun/jbm-framework-aliyun-oss/src/main/java/jbm/framework/aliyun/oss/AliyunOSSTemplate.java
@@ -1,13 +1,18 @@
 package jbm.framework.aliyun.oss;
 
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.BucketInfo;
+import com.jbm.util.PathUtils;
+import jodd.io.FileNameUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.InputStream;
+import java.text.MessageFormat;
 
 /**
  * @program: JBM
@@ -24,6 +29,14 @@ public class AliyunOSSTemplate {
     public AliyunOSSTemplate(OSSClient ossClient, String bucketName) {
         this.ossClient = ossClient;
         this.bucketName = bucketName;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public OSSClient getOssClient() {
+        return ossClient;
     }
 
     /**
@@ -59,7 +72,7 @@ public class AliyunOSSTemplate {
      *
      * @param is
      */
-    public String uploadStream(final InputStream is) {
+    public String uploadOpenStream(final InputStream is) {
         // 把字符串存入OSS，Object的名称为firstKey。详细请参看“SDK手册 > Java-SDK > 上传文件”。
         // 链接地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/upload_object.html?spm=5176.docoss/user_guide/upload_object
         final String key = IdUtil.objectId();
@@ -70,15 +83,74 @@ public class AliyunOSSTemplate {
 
 
     /**
+     * 上传流文件
+     *
+     * @param is
+     */
+    public String uploadOpenStream(final InputStream is, final String originalFilename) {
+        // 把字符串存入OSS，Object的名称为firstKey。详细请参看“SDK手册 > Java-SDK > 上传文件”。
+        // 链接地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/upload_object.html?spm=5176.docoss/user_guide/upload_object
+        final String path = newFileName(null, originalFilename);
+        ossClient.putObject(bucketName, path, is);
+        log.info("Object：{}存入OSS成功。", path);
+        return this.buildUrl(path);
+    }
+
+    /**
+     * 上传流文件
+     *
+     * @param is
+     */
+    public String uploadOpenStream(final InputStream is, final String group, final String originalFilename) {
+        // 把字符串存入OSS，Object的名称为firstKey。详细请参看“SDK手册 > Java-SDK > 上传文件”。
+        // 链接地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/upload_object.html?spm=5176.docoss/user_guide/upload_object
+        final String path = newFileName(group, originalFilename);
+        ossClient.putObject(bucketName, path, is);
+        log.info("Object：{}存入OSS成功。", path);
+        return this.buildUrl(path);
+    }
+
+    /**
      * 上传文件
      *
      * @param file
      */
-    public String uploadFile(final File file) {
-        final String key = IdUtil.objectId();
-        ossClient.putObject(bucketName, key, file);
-        log.info("Object：{}存入OSS成功。", key);
-        return key;
+    public String uploadOpenFile(final File file) {
+        final String path = file.getPath();
+        ossClient.putObject(bucketName, path, file);
+        log.info("Object：{}存入OSS成功。", path);
+        return this.buildUrl(path);
+    }
+
+    /**
+     * 构造一个新的文件名
+     *
+     * @param group
+     * @param originalFilename
+     * @return
+     */
+    private String newFileName(String group, String originalFilename) {
+        if (StrUtil.isBlank(originalFilename)) {
+            return IdUtil.objectId();
+        }
+        String fileName = originalFilename;
+        String extend = PathUtils.getExtension(fileName);// 获取文件扩展名
+        String newFileName = IdUtil.objectId() + "." + extend;
+        return FileNameUtil.concat(StrUtil.trimToEmpty(group), newFileName, true);
+
+    }
+
+
+    /**
+     * 创建一个链接
+     *
+     * @param path
+     * @return
+     */
+    private String buildUrl(String path) {
+        String temp = "https://{0}.{1}/{2}";
+        final String url = MessageFormat.format(temp, this.bucketName, ossClient.getEndpoint().getHost(), path);
+        return URLUtil.encode(url);
     }
 
 }
