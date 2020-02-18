159a126b0d16356e4adbd23f6993665cc2246542
numen06 <numen06@sina.com>
2020-02-18 08:23:23 +0800

    增加minio功能

---
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-minio/src/test/java/com/jbm/test/TestController.java
@@ -0,0 +1,53 @@
+package com.jbm.test;
+
+import com.jlefebure.spring.boot.minio.MinioException;
+import com.jlefebure.spring.boot.minio.MinioService;
+import io.minio.messages.Item;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * @program: JBM6
+ * @author: wesley.zhang
+ * @create: 2020-02-18 07:59
+ **/
+@RestController
+@RequestMapping("/files")
+public class TestController {
+
+    @Autowired
+    private MinioService minioService;
+
+
+    @GetMapping("/")
+    public List<Item> testMinio() throws MinioException {
+        return minioService.list();
+    }
+
+    @GetMapping("/{object}")
+    public void getObject(@PathVariable("object") String object, HttpServletResponse response) throws MinioException, IOException {
+        InputStream inputStream = minioService.get(Paths.get(object));
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+
+        // Set the content type and attachment header.
+        response.addHeader("Content-disposition", "attachment;filename=" + object);
+        response.setContentType(URLConnection.guessContentTypeFromName(object));
+
+        // Copy the stream to the response's output stream.
+        IOUtils.copy(inputStream, response.getOutputStream());
+        response.flushBuffer();
+    }
+}
