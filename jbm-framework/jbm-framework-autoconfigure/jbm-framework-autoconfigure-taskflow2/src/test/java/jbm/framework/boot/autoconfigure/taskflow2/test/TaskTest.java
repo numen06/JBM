b710f0fab3661303834eb82dfe86010db83a91fa
numen06 <numen06@sina.com>
2021-12-08 20:09:58 +0800

    增加任务调度2

---
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-taskflow2/src/test/java/jbm/framework/boot/autoconfigure/taskflow2/test/TaskTest.java
@@ -0,0 +1,40 @@
+package jbm.framework.boot.autoconfigure.taskflow2.test;
+
+
+import com.ebay.bascomtask.core.Orchestrator;
+import com.ebay.bascomtask.core.TaskInterface;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+interface IEchoTask extends TaskInterface<IEchoTask> {
+    CompletableFuture<String> echo(String s);
+}
+
+class EchoTask implements IEchoTask {
+    public CompletableFuture<String> echo(String s) {
+        return complete(s);
+    }
+}
+
+interface ICombinerTask extends TaskInterface<ICombinerTask> {
+    CompletableFuture<String> combine(CompletableFuture<String> a, CompletableFuture<String> b);
+}
+
+class CombinerTask implements ICombinerTask {
+    public CompletableFuture<String> combine(CompletableFuture<String> a, CompletableFuture<String> b) {
+        return complete(get(a) + ' ' + get(b));
+    }
+}
+
+public class TaskTest {
+    @Test
+    public void getMessage() throws ExecutionException, InterruptedException {
+        Orchestrator $ = Orchestrator.create();
+        CompletableFuture<String> left = $.task(new EchoTask()).echo("Hello");
+        CompletableFuture<String> right = $.task(new EchoTask()).echo("world");
+        CompletableFuture<String> combine = $.task(new CombinerTask()).combine(left, right);
+        System.out.println(combine.get());
+    }
+}
