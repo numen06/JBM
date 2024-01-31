f6cde8b5e40652e96a42eec58878d5f6e24afe53
numen06 <numen06@sina.com>
2024-01-31 12:45:37 +0800

    增数据库增删改查操作

--- jbm-util/src/main/java/com/jbm/util/db/load/SqlLoader.java
+++ jbm-util/src/main/java/com/jbm/util/db/load/SqlLoader.java
@@ -22,11 +22,13 @@ public class SqlLoader extends AbstractFileLoader {
         try {
             if (params.length == 1) {
                 String sql = SimpleTemplateUtils.renderStringTemplate(fileContent, params[0]);
-                sqlMeta = new SqlMeta(sql, CollUtil.newArrayList(params));
+                int paramCount = StrUtil.count(sql, "?");
+                sqlMeta = new SqlMeta(sql, CollUtil.newArrayList(params).subList(0, paramCount));
                 return sqlMeta;
             }
-            String sql =  SimpleTemplateUtils.renderStringTemplate(fileContent, params);
-            sqlMeta = new SqlMeta(sql, CollUtil.newArrayList(params));
+            String sql = SimpleTemplateUtils.renderStringTemplate(fileContent, params);
+            int paramCount = StrUtil.count(sql, "?");
+            sqlMeta = new SqlMeta(sql, CollUtil.newArrayList(params).subList(0, paramCount));
         } catch (Exception e) {
             log.error("转换sql失败", e);
         }
---
+++ jbm-util/src/test/resources/sqls/db-init.sql
@@ -0,0 +1,3 @@
+insert into student (id, age, name, time) values (1, 18, '张三', '2022-01-01 01:00:00' );
+insert into student (id, age, name, time) values (2, 19, '李四', '2022-01-02 02:00:00' );
+insert into student (id, age, name, time) values (3, 20, '牛二', '2022-01-03 03:00:00' );
