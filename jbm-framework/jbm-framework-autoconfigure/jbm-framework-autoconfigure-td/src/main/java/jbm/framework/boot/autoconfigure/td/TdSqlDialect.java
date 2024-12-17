ec7fe337fc0039efe3a2e4165d817edd398468b1
numen06 <numen06@sina.com>
2024-12-18 01:26:56 +0800

    Update TdSqlDialect.java

--- jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-td/src/main/java/jbm/framework/boot/autoconfigure/td/TdSqlDialect.java
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-td/src/main/java/jbm/framework/boot/autoconfigure/td/TdSqlDialect.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.db.*;
 import cn.hutool.db.dialect.impl.AnsiSqlDialect;
 import cn.hutool.db.sql.SqlBuilder;
+import cn.hutool.db.sql.SqlExecutor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +23,6 @@ public class TdSqlDialect  extends AnsiSqlDialect {
     public PreparedStatement psForInsert(Connection conn, Entity entity) throws SQLException {
         SqlBuilder insert = SqlBuilder.create(this.wrapper).insert(entity, this.dialectName());
         try {
-
             return StatementUtil.prepareStatement(false, conn, insert.build(), insert.getParamValueArray());
         } catch (SQLException e) {
             String sql = insert.build();
@@ -30,6 +30,7 @@ public class TdSqlDialect  extends AnsiSqlDialect {
             //替换第一个占位符，防止替换多次导致索引错乱
             sql = StrUtil.replace(sql, "?", "{}");
             sql = StrUtil.format(sql, insert.getParamValueArray());
+            SqlExecutor.execute( conn, sql);
             log.error("TDSQL方言插入数据异常:{}", sql, e);
             throw e;
         }
