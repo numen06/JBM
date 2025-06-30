ba8790ddacaf7463384b473033ddac331c61504e
numen06 <numen06@sina.com>
2025-06-30 14:18:38 +0800

    Update DroolsTest.java

--- jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-drools/src/test/java/com/jbm/drools/test/DroolsTest.java
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-drools/src/test/java/com/jbm/drools/test/DroolsTest.java
@@ -15,15 +15,15 @@ public class DroolsTest {
     public static void execute(KieContainer kc) {
         KieSession ksession = kc.newKieSession("HonestPoliticianKS");
 
-        final Politician p1 = new Politician("President of Umpa Lumpa", true);
-        final Politician p2 = new Politician("Prime Minster of Cheeseland", true);
-        final Politician p3 = new Politician("Tsar of Pringapopaloo", true);
-        final Politician p4 = new Politician("Omnipotence Om", true);
+//        final Politician p1 = new Politician("President of Umpa Lumpa", true);
+//        final Politician p2 = new Politician("Prime Minster of Cheeseland", true);
+//        final Politician p3 = new Politician("Tsar of Pringapopaloo", true);
+//        final Politician p4 = new Politician("Omnipotence Om", true);
 
-        ksession.insert(p1);
-        ksession.insert(p2);
-        ksession.insert(p3);
-        ksession.insert(p4);
+//        ksession.insert(p1);
+//        ksession.insert(p2);
+//        ksession.insert(p3);
+//        ksession.insert(p4);
 
         ksession.fireAllRules();
 
