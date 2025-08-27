8c54f251795b67d7ef801c9afbe8e0f16d902922
numen06 <numen06@sina.com>
2025-08-27 19:14:42 +0800

    Update JbmRequestTemplate.java

--- jbm-cluster/jbm-cluster-common/jbm-cluster-common-basic/src/main/java/com/jbm/cluster/common/basic/module/JbmRequestTemplate.java
+++ jbm-cluster/jbm-cluster-common/jbm-cluster-common-basic/src/main/java/com/jbm/cluster/common/basic/module/JbmRequestTemplate.java
@@ -2,11 +2,9 @@ package com.jbm.cluster.common.basic.module;
 
 import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.http.HttpRequest;
 import com.jbm.cluster.common.basic.module.request.ICustomizeRequest;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Request;
-import org.bouncycastle.cert.ocsp.Req;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
@@ -21,14 +19,22 @@ public class JbmRequestTemplate {
 
     public okhttp3.Response request(String url, String methodType, String jsonBody) throws UnknownHostException {
         ICustomizeRequest iCustomizeRequest = this.findCustomizeRequest(url);
-        return iCustomizeRequest.request(url, methodType, jsonBody);
+        if (iCustomizeRequest != null) {
+            return iCustomizeRequest.request(url, methodType, jsonBody);
+        } else {
+            throw new RuntimeException("未找到对应的请求方式");
+        }
     }
 
 
     public okhttp3.Response request(Request.Builder requestBuilder) {
         Request httpRequest = requestBuilder.build();
         ICustomizeRequest iCustomizeRequest = this.findCustomizeRequest(httpRequest.url().toString());
-        return iCustomizeRequest.request(httpRequest);
+        if (iCustomizeRequest != null) {
+            return iCustomizeRequest.request(httpRequest);
+        }else {
+            throw new RuntimeException("未找到对应的请求方式");
+        }
     }
 
 
