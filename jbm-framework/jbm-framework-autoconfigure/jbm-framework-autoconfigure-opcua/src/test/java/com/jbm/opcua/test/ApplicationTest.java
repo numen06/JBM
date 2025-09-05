b1f90a7c8d453c1f0f0a33e5171257b690392727
fengsheng <sheng.feng.96@icloud.com>
2025-09-05 14:13:52 +0800

    连接OPC UA增加用户认证方式

--- jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-opcua/src/test/java/com/jbm/opcua/test/ApplicationTest.java
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-opcua/src/test/java/com/jbm/opcua/test/ApplicationTest.java
@@ -1,15 +1,20 @@
 package com.jbm.opcua.test;
 
+import com.jbm.framework.opcua.OpcUaTemplate;
 import jbm.framework.boot.autoconfigure.opcua.OpcUaConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = OpcUaConfiguration.class)
 public class ApplicationTest {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationTest.class, args);
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ApplicationTest.class, args);
+        OpcUaTemplate opcUaTemplate = applicationContext.getBean(OpcUaTemplate.class);
+        System.out.println("s1 = " + opcUaTemplate.readItem("s1", "PLC_HeartBeat"));
+        System.out.println("s2 = " + opcUaTemplate.readItem("s2", "PLC_HeartBeat"));
     }
 }
