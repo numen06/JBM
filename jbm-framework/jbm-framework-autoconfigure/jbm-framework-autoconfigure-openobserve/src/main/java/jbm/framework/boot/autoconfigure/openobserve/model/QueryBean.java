c7b97ebd4e834173cd7968d9716eebed8968e08e
numen06 <numen06@sina.com>
2025-09-17 19:01:40 +0800

    提交新的日志格式

--- jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-openobserve/src/main/java/jbm/framework/boot/autoconfigure/openobserve/model/QueryBean.java
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-openobserve/src/main/java/jbm/framework/boot/autoconfigure/openobserve/model/QueryBean.java
@@ -3,6 +3,8 @@ package jbm.framework.boot.autoconfigure.openobserve.model;
 import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class QueryBean {
 
@@ -12,5 +14,29 @@ public class QueryBean {
     private Integer timeout = 0;
     @JSONField(name = "query")
     private Query query = new Query();
+
+    private String statement;
+    private Map<String, Object> params;
+
+    public static QueryBean defaultQuery() {
+        QueryBean queryBean = new QueryBean();
+        queryBean.setQuery(new Query());
+        queryBean.getQuery().setSize(100);
+        queryBean.getQuery().setFrom(0);
+        return queryBean;
+    }
+
+    public static QueryBean defaultQuery(String statement) {
+        QueryBean queryBean = QueryBean.defaultQuery();
+        queryBean.setStatement(statement);
+        return queryBean;
+    }
+
+    public static QueryBean defaultQuery(String statement, Map<String, Object> params) {
+        QueryBean queryBean = QueryBean.defaultQuery();
+        queryBean.setStatement(statement);
+        queryBean.setParams(params);
+        return queryBean;
+    }
 
 }
