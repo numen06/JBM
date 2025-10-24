7192d12779c082a00ecf987a45d1b75704a700c1
numen06 <numen06@sina.com>
2025-10-24 12:13:37 +0800

    增加路劲配置

--- jbm-cluster/jbm-cluster-platform/jbm-cluster-platform-auth/src/main/java/com/jbm/cluster/auth/controller/LoginController.java
+++ jbm-cluster/jbm-cluster-platform/jbm-cluster-platform-auth/src/main/java/com/jbm/cluster/auth/controller/LoginController.java
@@ -5,12 +5,14 @@ import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
-import java.util.UUID;
 
 @Controller
 @Api(tags = "登录页面")
@@ -28,13 +30,28 @@ public class LoginController {
             @RequestParam(value = "redirect_uri", required = false) String redirectUri,
             @RequestParam(value = "scope", required = false, defaultValue = "") String scope,
             @RequestParam(value = "state", required = false) String state,
-            Map<String, Object> model) {
+            Map<String, Object> model, HttpServletRequest request) {
 
-        if (StrUtil.isEmpty( state)){
+        if (StrUtil.isEmpty(state)) {
             state = IdUtil.fastSimpleUUID();
         }
-        if(StrUtil.isEmpty(redirectUri)){
-            redirectUri = "/oauth2/thirdparty/local/callback";
+        //获取当前请求的URL拼接前缀
+        if (StrUtil.isEmpty(redirectUri)) {
+            //获取原始访问链接，拼接前缀，链接可能通过了几次转发
+            String referer = request.getHeader("referer");
+            if (StrUtil.isNotEmpty(referer)) {
+                String url = referer;
+                int index = url.indexOf("oauth2/authorize");
+                if (index > 0) {
+                    url = url.substring(0, index);
+                }
+                redirectUri = url + "/oauth2/thirdparty/local/callback";
+            }else{
+                String url = request.getRequestURL().toString();
+                String pixel = url.substring(0, url.indexOf("oauth2/authorize"));
+                redirectUri = pixel + "/oauth2/thirdparty/local/callback";
+            }
+
         }
         if (StrUtil.isEmpty(clientId)) {
             clientId = "2zZw3p6YYtTLcOqRMpZbYrhX";
@@ -51,7 +68,7 @@ public class LoginController {
             // 未登录，显示登录页面
             return "oauth2_login";
         }
-        
+
         // 已登录，重定向到HTML授权页面
         return "oauth2_authorize";
     }
