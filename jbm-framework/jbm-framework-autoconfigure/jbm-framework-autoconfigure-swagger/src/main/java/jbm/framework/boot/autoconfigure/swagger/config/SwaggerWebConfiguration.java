4eeeca8466d6e92d85b831c2ff03e9a0e0a1bbea
numen06 <numen06@sina.com>
2025-11-07 21:09:13 +0800

    swagger加上基础认证

--- jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-swagger/src/main/java/jbm/framework/boot/autoconfigure/swagger/config/SwaggerWebConfiguration.java
+++ jbm-framework/jbm-framework-autoconfigure/jbm-framework-autoconfigure-swagger/src/main/java/jbm/framework/boot/autoconfigure/swagger/config/SwaggerWebConfiguration.java
@@ -1,5 +1,9 @@
 package jbm.framework.boot.autoconfigure.swagger.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +15,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class SwaggerWebConfiguration implements WebMvcConfigurer {
+
+    @Autowired(required = false)
+    private SwaggerProperties swaggerProperties;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         /** swagger-ui 地址 */
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+    }
+
+    /**
+     * 注册Swagger基础认证过滤器
+     */
+    @Bean
+    @ConditionalOnBean(SwaggerProperties.class)
+    public FilterRegistrationBean<SwaggerBasicAuthFilter> swaggerBasicAuthFilter() {
+        FilterRegistrationBean<SwaggerBasicAuthFilter> registration = new FilterRegistrationBean<>();
+        SwaggerBasicAuthFilter filter = new SwaggerBasicAuthFilter(swaggerProperties.getBasicAuth());
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/swagger-ui/**", "/v2/api-docs/**", "/swagger-resources/**", "/swagger-ui.html");
+        registration.setName("swaggerBasicAuthFilter");
+        registration.setOrder(1);
+        return registration;
     }
 }
