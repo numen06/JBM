7d2d4042b2092cdfa00a25eef998f5f10380708a
numen06 <numen06@sina.com>
2025-11-15 03:23:17 +0800

    Update PushMessageTest.java

--- jbm-cluster/jbm-cluster-platform/jbm-cluster-platform-push/src/main/java/com/jbm/cluster/push/message/PushMessageTest.java
+++ jbm-cluster/jbm-cluster-platform/jbm-cluster-platform-push/src/main/java/com/jbm/cluster/push/message/PushMessageTest.java
@@ -33,7 +33,7 @@ public class PushMessageTest {
      */
     @Autowired
     private JbmClusterBusinessEventTemplate jbmClusterBusinessEventTemplate;
-    @Scheduled(cron = "0/5 * * * * ?")
+    // @Scheduled(cron = "0/5 * * * * ?")
     public void testLogSend(){
         testMdcLogging();
     }
