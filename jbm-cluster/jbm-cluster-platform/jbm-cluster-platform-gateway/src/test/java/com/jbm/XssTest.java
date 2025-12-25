90b6713d487dc5aa81fea88b28b328ba593890be
numen06 <numen06@sina.com>
2025-12-25 14:10:22 +0800

    增加Xss过滤

---
+++ jbm-cluster/jbm-cluster-platform/jbm-cluster-platform-gateway/src/test/java/com/jbm/XssTest.java
@@ -0,0 +1,25 @@
+package com.jbm;
+
+import cn.hutool.core.io.resource.ResourceUtil;
+import com.jbm.cluster.platform.gateway.filter.XssFilter;
+import org.junit.jupiter.api.Test;
+
+public class XssTest {
+
+    @Test
+    public void testClear() {
+        String html = ResourceUtil.readUtf8Str("test.html");
+        //获取body标签的内容
+        String softHtml = XssFilter.cleanHtmlTag(html);
+        //对比一下两者差异
+        System.out.println(html.equals(softHtml));
+        System.out.println("--------------------");
+        System.out.println(html);
+        System.out.println("--------------------");
+        System.out.println(softHtml);
+        System.out.println("--------------------");
+        //查询一下差异点
+        System.out.println(html.indexOf(softHtml));
+
+    }
+}
