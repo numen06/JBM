8563c59917f3dad7f038f3e97d085e5cbe90dc1c
numen06 <numen06@sina.com>
2026-05-21 09:40:00 +0800

    提交

---
+++ jbm-cluster/jbm-cluster-platform/jbm-cluster-common-mysql/src/main/java/com/jbm/cluster/common/mysql/service/impl/BaseAppConfigServiceImpl.java
@@ -0,0 +1,16 @@
+package com.jbm.cluster.common.mysql.service.impl;
+
+import com.jbm.cluster.api.entitys.basic.BaseAppConfig;
+import com.jbm.cluster.common.mysql.service.BaseAppConfigService;
+import com.jbm.framework.service.mybatis.MasterDataServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: auto generate by jbm
+ * @Create: 2026-05-21 03:40:20
+ */
+@Service
+public class BaseAppConfigServiceImpl extends MasterDataServiceImpl<BaseAppConfig> implements BaseAppConfigService {
+
+
+}
